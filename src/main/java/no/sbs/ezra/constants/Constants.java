e1477dac8837a6a9789d3ddbf3276fe9526a638a
SindreBroholm <sindreset@gmail.com>
2021-05-06 11:09:12 +0200

    BETA STAGE 1.8.1: -deploy test: 8 java.runtime.version=14

--- src/main/java/no/sbs/ezra/constants/Constants.java
+++ src/main/java/no/sbs/ezra/constants/Constants.java
@@ -3,4 +3,5 @@ package no.sbs.ezra.constants;
 public class Constants {
     public static final String HOME_PAGE = "redirect:/";
     public static final String FAMILY_PAGE = "redirect:/family";
+
 }
